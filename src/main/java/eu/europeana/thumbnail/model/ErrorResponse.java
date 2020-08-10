703165043caebd5b0fa2cf9f6479640e7a6f4d7a
srishti <srishti.singh@europeana.eu>
2020-08-10 15:39:40 +0200

    added comments

--- src/main/java/eu/europeana/thumbnail/model/ErrorResponse.java
+++ src/main/java/eu/europeana/thumbnail/model/ErrorResponse.java
@@ -2,15 +2,25 @@ package eu.europeana.thumbnail.model;
 
 import java.util.List;
 
+/**
+ *  Error Response class
+ *  Created by Srishti Singh
+ */
 public class ErrorResponse {
+
+    private  String       message;
+    private  List<String> details;
+
+    /**
+     * Constructor used for initialization
+     * @param message
+     * @param details
+     */
     public ErrorResponse(String message, List<String> details) {
         super();
         this.message = message;
         this.details = details;
     }
-
-    private String       message;
-    private List<String> details;
 
     public String getMessage() {
         return message;
