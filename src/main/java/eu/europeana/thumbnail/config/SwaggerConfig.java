56267f8a08dd1dc697381b5b7679a1679e4a15b0
Patrick Ehlert <patrick.ehlert@europeana.eu>
2020-09-10 10:33:10 +0200

    Fix Swagger and CORS configuration + add unit tests

--- src/main/java/eu/europeana/thumbnail/config/SwaggerConfig.java
+++ src/main/java/eu/europeana/thumbnail/config/SwaggerConfig.java
@@ -2,6 +2,9 @@ package eu.europeana.thumbnail.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -13,33 +16,55 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 import java.util.Collections;
 
 /**
- * Configures swagger on all requests. Swagger Json file is availabe at <hostname>/v2/api-docs and if you add
- * Swagger UI package the <hostname>/swagger-ui.html
+ * Configures swagger on all requests. Swagger Json file is availabe at <hostname>/v2/api-docs
+ * and Swagger UI at <hostname>/swagger-ui.html
  *
  * @author Srishti Singh
  * Created on 12-08-2019
+ * @author Patrick Ehlert (major refactoring in sep 2020)
  */
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
 
-    @Bean
-    public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                                                      .apis(RequestHandlerSelectors.basePackage("eu.europeana.thumbnail"))
-                                                      .paths(PathSelectors.any())
-                                                      .build();
+    private BuildInfo buildInfo;
+
+    public SwaggerConfig(BuildInfo buildInfo) {
+        this.buildInfo = buildInfo;
     }
 
-    @SuppressWarnings("squid:UnusedPrivateMethod")
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("eu.europeana.thumbnail"))
+                .paths(PathSelectors.any())
+                .build();
+    }
+
     private ApiInfo apiInfo() {
-        return new ApiInfo("Europeana-Thumbnail-API",
-                           "Generate thumbnail",
-                           null,
-                           null,
-                           new Contact("APIs team", "www.europeana.eu", "api@europeana.eu"),
-                           "EUPL 1.2",
-                           "API license URL",
-                           Collections.emptyList());
+        return new ApiInfo(
+                buildInfo.getAppName(),
+                buildInfo.getAppDescription(),
+                buildInfo.getAppVersion() + "(build " + buildInfo.getBuildNumber() + ")",
+                null,
+                new Contact("API team", "https://api.europeana.eu", "api@europeana.eu"),
+                "EUPL 1.2", "https://www.eupl.eu", Collections.emptyList());
+    }
+
+    /**
+     * For some reason the default Spring-Boot way of configuring Cors using the CorsFilter in WebMvcConfig class doesn't
+     * work, so we configure it separately for Swagger here (solution copied from https://stackoverflow.com/a/45685909)
+     */
+    @Bean
+    public CorsFilter corsFilterSwagger() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.addAllowedOrigin("*");
+        config.setMaxAge(1000L);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/v2/api-docs", config);
+        return new CorsFilter(source);
     }
 }
