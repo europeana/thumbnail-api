de7b26309c1a2ef014685f4c5a3019175c0c4d5f
Patrick Ehlert <patrick.ehlert@europeana.eu>
2024-10-14 10:44:52 +0200

    Revise read/write/delete

--- src/main/java/eu/europeana/thumbnail/web/PVController.java
+++ src/main/java/eu/europeana/thumbnail/web/PVController.java
@@ -16,15 +16,6 @@ public class PVController {
 
     private static final String PV_FOLDER = "/mnt/pvdata";
 
-    private static class TextFile {
-        String fileName;
-        String contents;
-
-        public TextFile(String fileName, String contents) {
-            this.fileName = fileName;
-            this.contents = contents;
-        }
-    }
 
     @RequestMapping("list")
     public Set<String> list() throws IOException {
@@ -37,23 +28,23 @@ public class PVController {
         }
     }
 
-    @PostMapping("write")
-    public String write(@RequestBody TextFile textFile) throws IOException {
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(PV_FOLDER + "/" + textFile.fileName))) {
-            writer.write(textFile.contents);
+    @PostMapping("write/{fileName}")
+    public String write(@RequestParam String fileName, @RequestBody String contents) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(PV_FOLDER + "/" + fileName))) {
+            writer.write(contents);
             return "Write successful";
         }
     }
 
-    @RequestMapping("read")
+    @RequestMapping("read/{fileName}")
     public String read(@RequestParam String fileName) throws IOException {
         try (BufferedReader reader = new BufferedReader(new FileReader(PV_FOLDER + "/" + fileName))) {
             return read(fileName);
         }
     }
 
-    @DeleteMapping("delete")
-    public String delete(String fileName) {
+    @DeleteMapping("delete/{fileName}")
+    public String delete(@RequestParam String fileName) {
         File file = new File(PV_FOLDER + "/" + fileName);
         file.delete();
         return "Delete successful";
