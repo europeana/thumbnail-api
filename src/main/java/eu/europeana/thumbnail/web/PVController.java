0b364be5092c8f0ec55fb5eb0e92fced1f8a55e0
Patrick Ehlert <patrick.ehlert@europeana.eu>
2024-10-14 10:36:47 +0200

    Read, write and delete file

--- src/main/java/eu/europeana/thumbnail/web/PVController.java
+++ src/main/java/eu/europeana/thumbnail/web/PVController.java
@@ -1,9 +1,8 @@
 package eu.europeana.thumbnail.web;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -17,6 +16,16 @@ public class PVController {
 
     private static final String PV_FOLDER = "/mnt/pvdata";
 
+    private static class TextFile {
+        String fileName;
+        String contents;
+
+        public TextFile(String fileName, String contents) {
+            this.fileName = fileName;
+            this.contents = contents;
+        }
+    }
+
     @RequestMapping("list")
     public Set<String> list() throws IOException {
         try (Stream<Path> stream = Files.list(Paths.get(PV_FOLDER))) {
@@ -26,5 +35,27 @@ public class PVController {
                     .map(Path::toString)
                     .collect(Collectors.toSet());
         }
+    }
+
+    @PostMapping("write")
+    public String write(@RequestBody TextFile textFile) throws IOException {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(PV_FOLDER + "/" + textFile.fileName))) {
+            writer.write(textFile.contents);
+            return "Write successful";
+        }
+    }
+
+    @RequestMapping("read")
+    public String read(@RequestParam String fileName) throws IOException {
+        try (BufferedReader reader = new BufferedReader(new FileReader(PV_FOLDER + "/" + fileName))) {
+            return read(fileName);
+        }
+    }
+
+    @DeleteMapping("delete")
+    public String delete(String fileName) {
+        File file = new File(PV_FOLDER + "/" + fileName);
+        file.delete();
+        return "Delete successful";
     }
 }
