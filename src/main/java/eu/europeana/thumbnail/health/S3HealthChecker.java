46052d66a3185ee1d68fdd8d3e53733c406c5798
Luthien-in-edhil <luthien@parendili.org>
2026-07-15 18:36:10 +0200

    cleaned up unnecessary classes and fixed the failing integration test

--- src/main/java/eu/europeana/thumbnail/health/S3HealthChecker.java
+++ src/main/java/eu/europeana/thumbnail/health/S3HealthChecker.java
@@ -5,6 +5,7 @@ import eu.europeana.thumbnail.service.MediaReadStorageService;
 import eu.europeana.thumbnail.service.impl.UploadImageServiceImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.boot.actuate.autoconfigure.health.ConditionalOnEnabledHealthIndicator;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
@@ -17,17 +18,17 @@ import java.util.List;
 import java.util.Map;
 
 @Component("s3")
+@ConditionalOnEnabledHealthIndicator("s3") // Enables/disables this indicator via properties
 public class S3HealthChecker implements HealthIndicator {
 
     private static final Logger LOG = LogManager.getLogger(S3HealthChecker.class);
     private final Map<String, MediaReadStorageService> storageNameToService;
     private final Map<String, String> serviceBucketMap;
-    private final ApplicationStateManager stateManager;
 
-    public S3HealthChecker(StorageRoutes storageRoutes, ApplicationStateManager stateManager) {
+    // Cleaned up: No longer depends on ApplicationStateManager
+    public S3HealthChecker(StorageRoutes storageRoutes) {
         this.storageNameToService = storageRoutes.getMediaStorageServices();
         this.serviceBucketMap = storageRoutes.getServiceBucketMap();
-        this.stateManager = stateManager;
     }
 
     @Override
@@ -60,14 +61,15 @@ public class S3HealthChecker implements HealthIndicator {
                             sb.append(",");
                         }
                         sb.append(" ").append(entry.getKey()).append("/").append(entry.getValue());
+
+                        System.out.println(entry.getKey() + " / " + entry.getValue() + " is not available");
                         LOG.error("{} / {} is not available", entry.getKey(), entry.getValue());
                     }
                     serviceBucketHealthMap.put(entry.getKey() + "/" + entry.getValue(), bucketUp);
                 }
             }
-            sb.append(nrDown > 1 ? " is" : " are").append(" not available").append(", shutting down");
+            sb.append(nrDown > 1 ? " is" : " are").append(" not available");
             if (serviceBucketHealthMap.containsValue(false)){
-                stateManager.markBroken();
                 LOG.error(sb);
                 return Health.down().withDetail("S3 Thumbnail health", sb.toString()).build();
             } else {
@@ -78,6 +80,3 @@ public class S3HealthChecker implements HealthIndicator {
         }
     }
 }
-
-
-
